edd16ac6d029fd56fefc77edaa755c296b65c596
Nikolaos Paschos <nikos.paschos2@gmail.com>
2019-11-30 15:30:47 +0200

    Initial commit :sparkles:

---
+++ fetch_data.py
@@ -0,0 +1,31 @@
+# requires pip3 install cdsapi
+# then create $(HOME)/.cdsapirc containing the credentials
+# differnet ToS must be accepted depending on the requests dataset
+
+import cdsapi
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--outfile', '-of', type=str, default='download')
+parser.add_argument('--dataset',
+                    '-d',
+                    type=str,
+                    default='reanalysis-era5-land-monthly-means')
+parser.add_argument('--format', '-f', type=str, default='grib')
+
+args = parser.parse_args()
+
+c = cdsapi.Client()
+
+outfile_name = '{}.{}'.format(args.outfile, args.format)
+dataset_options = {
+    'format': args.format,
+    'variable':
+    ['2m_dewpoint_temperature', '2m_temperature', 'forecast_albedo'],
+    'product_type': 'monthly_averaged_reanalysis',
+    'time': '00:00',
+    'year': '2019',
+    'month': '04'
+}
+
+c.retrieve(args.dataset, dataset_options, outfile_name)
