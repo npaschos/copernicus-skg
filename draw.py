10d4456edd174ba1b11e2d3e24a7094e8678873e
Nikolaos Paschos <nikos.paschos2@gmail.com>
2019-12-01 11:04:38 +0200

    :zap: demo material completed

--- draw.py
+++ draw.py
@@ -8,6 +8,29 @@ from PIL import Image
 from util import read_grib
 from data_processing import data2airQ
 
+
+def show_image(data, img):
+    # https://seaborn.pydata.org/tutorial/color_palettes.html
+    # sns.color_palette("RdBu_r", 7)
+    # sns.color_palette("coolwarm", 7)
+    hmax = sns.heatmap(
+        # cmap=hot,
+        data,
+        cmap=sns.color_palette("RdBu_r", 7),
+        alpha=0.5,  # whole heatmap is translucent
+        annot=False,
+        zorder=2,
+    )
+    # plt.show()
+
+    hmax.imshow(img,
+                aspect=hmax.get_aspect(),
+                extent=hmax.get_xlim() + hmax.get_ylim(),
+                zorder=1)  #put the map under the heatmap
+
+    plt.show()
+
+
 img = Image.open('frankfurt.png')
 era_hourly = read_grib('era5-0000-1200.grib')
 skin_ds = read_grib('skin.grib')
@@ -34,58 +57,47 @@ frankfurt_skin = list()
 for lat in skin_datay:
     frankfurt_skin.append(lat[84:91])
 
+skin_avg = numpy.average(frankfurt_skin)
 skin_min = numpy.min(frankfurt_skin)
 skin_max = numpy.max(frankfurt_skin)
 skin_datay = None
 
 frankfurt_day = frankfurt_day - day_avg
 frankfurt_night = frankfurt_night - night_avg
+frankfurt_skin = frankfurt_skin - skin_avg
 
 frankfurt_hourly = 0.65 * frankfurt_night + 0.35 * frankfurt_day
-frankfurt_hourly_norm = list()
-frankfurt_skin_norm = (100 - 0) / (skin_max - skin_min) * (frankfurt_skin -
-                                                           skin_min)
+show_image(frankfurt_hourly, img)
 
-frankfurt_hourly = frankfurt_hourly.reshape((28))
-for i in range(len(frankfurt_hourly)):
-    if frankfurt_hourly[i] > 0 and frankfurt_hourly[i] <= 1:
-        frankfurt_hourly_norm.append((25 - 0) * (frankfurt_hourly[i] - 0) /
-                                     (1 - 0) + 0)
-    elif frankfurt_hourly[i] > 1 and frankfurt_hourly[i] <= 3:
-        frankfurt_hourly_norm.append((50 - 25) * (frankfurt_hourly[i] - 1) /
-                                     (3 - 1) + 25)
-    elif frankfurt_hourly[i] > 3 and frankfurt_hourly[i] <= 5:
-        frankfurt_hourly_norm.append((75 - 50) * (frankfurt_hourly[i] - 3) /
-                                     (5 - 3) + 50)
-    elif frankfurt_hourly[i] > 5 and frankfurt_hourly[i] <= 12:
-        frankfurt_hourly_norm.append((100 - 75) * (frankfurt_hourly[i] - 5) /
-                                     (12 - 5) + 75)
+t_comparison = numpy.subtract(frankfurt_hourly, frankfurt_skin)
+
+
+t_comparison = numpy.abs(t_comparison.reshape((28)))
+t_comparison_norm = list()
+for i in range(len(t_comparison)):
+    if t_comparison[i] <= 1:
+        t_comparison_norm.append((25 - 0) * (t_comparison[i] - 0) / (1 - 0) +
+                                 0)
+    elif t_comparison[i] > 1 and t_comparison[i] <= 3:
+        t_comparison_norm.append((50 - 25) * (t_comparison[i] - 1) / (3 - 1) +
+                                 25)
+    elif t_comparison[i] > 3 and t_comparison[i] <= 5:
+        t_comparison_norm.append((75 - 50) * (t_comparison[i] - 3) / (5 - 3) +
+                                 50)
+    elif t_comparison[i] > 5 and t_comparison[i] <= 12:
+        t_comparison_norm.append((100 - 75) * (t_comparison[i] - 5) /
+                                 (12 - 5) + 75)
     else:
-        frankfurt_hourly_norm.append(101)
+        t_comparison_norm.append(101)
 
-frankfurt_hourly_norm = numpy.reshape(frankfurt_hourly_norm, (4, 7))
+t_comparison_norm = numpy.reshape(t_comparison_norm, (4, 7))
+
+show_image(t_comparison_norm, img)
 
 air_q = data2airQ()
 
-# total_frankfurt = (frankfurt_hourly_norm + air_q) / 2
-total_frankfurt = (frankfurt_hourly_norm + air_q) * 0.4 + 0.2 * frankfurt_skin_norm
+show_image(air_q, img)
 
-# https://seaborn.pydata.org/tutorial/color_palettes.html
-# sns.color_palette("RdBu_r", 7)
-# sns.color_palette("coolwarm", 7)
-hmax = sns.heatmap(
-    total_frankfurt,
-    # cmap=hot,
-    cmap=sns.color_palette("RdBu_r", 7),
-    alpha=0.5,  # whole heatmap is translucent
-    annot=False,
-    zorder=2,
-)
-# plt.show()
+total_frankfurt = (t_comparison_norm + air_q) / 2
 
-hmax.imshow(img,
-            aspect=hmax.get_aspect(),
-            extent=hmax.get_xlim() + hmax.get_ylim(),
-            zorder=1)  #put the map under the heatmap
-
-plt.show()
+show_image(total_frankfurt, img)
